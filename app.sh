72b32bc5858b4d8fc75ae047f25b82adf0ca7b9e
Reshma <resh2785@gmail.com>
2024-02-16 23:39:50 +0000

    Adding all code

---
+++ app.sh
@@ -0,0 +1,4 @@
+#This is a executable script to initialise the app
+page1='www.sample.com'
+page2='www.sample.com/tab1'
+page3='www.sample.com/tab2'
---
+++ updatepage.sh
@@ -0,0 +1,6 @@
+#This is the script for updating the page
+count = 0
+if variable == 'Update'
+    count = `count + 1`
+else
+    echo count
